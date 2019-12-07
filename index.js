1b8f9ccf89b29de810bc1c8fda3e69e1dbf7b0ca
K0TLIAR0VA <1914820@gmail.com>
2019-12-07 18:05:28 +0200

    add header, footer

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-import '/sass/custom.sass'
+import '/sass/custom.sass';
+/*import Vue from 'vue';
+import App from './components/App.vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const app = new Vue(App);
+app.$mount('#app');
+window.app = app;*/
